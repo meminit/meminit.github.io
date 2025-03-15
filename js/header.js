c2247dfb07c008881e3eab1e8b9174a1fffe64e2
phillipdelphias <phillipdelphias@gmail.com>
2025-03-15 20:23:32 +1100

    add function to buttons once loaded header

--- js/header.js
+++ js/header.js
@@ -12,6 +12,15 @@ async function loadHeader() {
 
    
     document.body.innerHTML += res
+
+    const btnScriptFetch = await fetch('/js/pages.js', { 
+        method: 'GET'
+    });
+
+    const btnScript = await btnScriptFetch.text()
+
+    // yeah yeah I really dont care what you say about eval I'm using it
+    eval(btnScript)
 }
 
 if (!header) {
