f57afac72d97b2249dfdf9ebfe324f24227daf24
phillipdelphias <phillipdelphias@gmail.com>
2025-03-15 21:45:26 +1100

    testing new mobile header

--- js/header.js
+++ js/header.js
@@ -3,17 +3,85 @@
 const header = document.querySelector('header')
 
 async function loadHeader() {
-    const newHeaderText = await fetch('./header.html', () => { 
+    const newHeaderText = await fetch('/header.html', () => { 
         method: 'GET'
     });
 
     const res = await newHeaderText.text()
 
-
+    const div = document.createElement('div')
    
-    document.body.innerHTML += res
+    const css = document.createElement('style')
 
-    const btnScriptFetch = await fetch('./js/pages.js', { 
+    css.innerHTML = `
+        .testMobileHeader {
+            display: none;
+        }
+
+        @media (orientation: portrait) {
+            .MainHeader {
+                display: none;
+            }
+            
+            .testMobileHeader {
+                display: flex;
+                flex-direction: row;
+                justify-content: end;
+                align-items: center;
+                height: 100px;
+            }
+
+            .openButton {
+                max-height: 100%;
+                max-width: 90px;
+                width: 90px;
+                margin-right: 20px;
+                height: 90px;
+                background-color: rgba(1,1, 1, 0);
+                border: none;
+                font-size: xx-large;
+                transition: all 0.2s ease;
+                z-index: 25;
+
+            }
+
+            .openButton:hover {
+                
+                /* transform: rotateZ(90deg); */
+            }
+
+
+        }
+
+        .mobileMenu {
+            background-color: white;
+            position: absolute;
+            z-index: 10;
+            width: 100%;
+            height: 0px;
+            display: none;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            opacity: 0;
+            transition: all 0.2s ease;
+        }
+
+        .defaultLink {
+            color: black;
+            text-decoration: none;
+            font-size: xx-large;
+
+        }
+`
+    
+    document.head.appendChild(css)
+    
+    div.innerHTML += res
+
+    document.prepend(div)
+
+    const btnScriptFetch = await fetch('s/js/pages.js', { 
         method: 'GET'
     });
 
@@ -22,6 +90,29 @@ async function loadHeader() {
     const scriptInHTML = document.createElement('script')
 
     scriptInHTML.innerText = btnScript
+
+    scriptInHTML.innerText += `
+        let open = false
+
+        document.querySelector('.openButton').addEventListener('click', () => {
+            if (!open) {
+                document.querySelector('.mobileMenu').style.display = 'flex'
+                document.querySelector('.mobileMenu').style.opacity = '1'
+                document.querySelector('.openButton').style.transform = 'rotateZ(90deg)'
+                open = true
+                console.log('Open Now')
+                return
+            }
+
+            if (open) {
+                document.querySelector('.mobileMenu').style.display = 'none'
+                              document.querySelector('.mobileMenu').style.opacity = '0'
+                document.querySelector('.openButton').style.transform = 'rotateZ(0deg)'
+                open = false
+                console.log('Closed Now')
+                return
+            }
+        });`
 
 }
 
