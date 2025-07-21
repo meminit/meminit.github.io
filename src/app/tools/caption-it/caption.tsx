f123a9c5b4489719eef957a0cd9f0d777ce66ff7
phillipdelphias <phillipdelphias@gmail.com>
2025-07-21 17:21:09 +1000

    Fix breaking of lines

--- src/app/tools/caption-it/caption.tsx
+++ src/app/tools/caption-it/caption.tsx
@@ -29,11 +29,11 @@ export default function load() {
                     <input className="outline-0 bg-zinc-950 p-2 rounded-lg not-md:w-full" type="text" placeholder="Caption..." onChange={(e) => setCaption(e.target.value)}></input>
                 </div>
                 <div className="w-full flex flex-col gap-2">
-                    <div className="w-full" id="captioned">
+                    <div className="w-full max-w-[670px]" id="captioned">
                         <div className="w-full bg-white font-bold p-2">
-                            <p className="text-black text-center text-5xl text-ellipsis">{caption}</p>
+                            <p className="text-black text-center text-5xl break-words">{caption}</p>
                         </div>
-                        <img src={image} alt="Upload Image Placeholder"></img>
+                        <img src={image} className="w-full object-contain" alt="Upload Image Placeholder"></img>
                     </div>
                     <button className="w-full bg-zinc-950 p-2 rounded-lg text-zinc-600 hover:text-zinc-200 transition-all cursor-pointer" onClick={() => download(document.getElementById('captioned') as HTMLElement)}><i className="fa-solid fa-download"></i> Download</button>
                 </div>
